7a912195ccb2dcdb3daffe24586e94272b9149ef
Bellatrix-Salungweni1 <bellatrixsalungweni106@student.unsrat.ac.id>
2022-12-02 23:46:34 +0800

    Menambahkan super hard req

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ var dataGagal = document.querySelector("#data-gagal");
 var myForm = document.getElementById("myform");
 var btnSubmit = document.querySelector('#btn-submit');
 var btnClose = document.querySelector('#closeBtn')
+const chart = document.querySelector('.chart');
+const dtNow = document.querySelector('#data1')
 const chart1 = document.querySelector('#chart1');
 const chart2 = document.querySelector('#chart2');
 const chart3 = document.querySelector('#chart3');
 const chart4 = document.querySelector('#chart4');
-const popUpBg = document.querySelector('#popUpBg');
-const popUpChart = document.querySelector('#popUpChart');
+
 const options = {
   method: "GET",
   headers: {
@@ -19,13 +20,13 @@ const options = {
 
 addZero = function(num){
   return ('0'+num).slice(-2);
-}
+ }
 
 const past7Days = [...Array(7).keys()].map(index => {
-const date = new Date();
-date.setDate(date.getDate() - index);
-const day = date.getFullYear() + '-' +addZero(date.getMonth()+1) + '-' + addZero(date.getDate());
-return day;
+  const date = new Date();
+  date.setDate(date.getDate() - index);
+ const day = date.getFullYear() + '-' +addZero(date.getMonth()+1) + '-' + addZero(date.getDate());
+ return day;
 });
 
 myForm.addEventListener("submit", function (event) {
@@ -46,8 +47,41 @@ myForm.addEventListener("submit", function (event) {
       .then((response) => {
         hideLoading()
         showDataSuccess(response);
-        popUpBg.classList.add('active');
-        popUpChart.classList.add('active');
+        btnClose.scrollIntoView()
+
+      })
+    .catch((err) => {
+      const er = past7Days.includes(inputTanggal)
+      if (er === false){
+        Swal.fire({
+          icon: "warning",
+          title: "Attention",
+          text: "Please enter the date within the last 7 days!!",
+        });
+        return false;
+      }
+      else{
+        Swal.fire({
+          icon: "warning",
+          title: "Attention",
+          text: "The country/city you are looking for was not found!!",
+        });
+        return false;
+      }
+    });
+  }
+});
+
+function showDataSuccess(response) {
+  document.querySelector("#Negara").innerHTML = response.location.name;
+  document.querySelector("#tanggal").innerHTML = response.forecast.forecastday[0].date;
+  document.querySelector("#condition").innerHTML = response.forecast.forecastday[0].hour[23].condition.text;
+  var icon = 'https:'+ response.forecast.forecastday[0].hour[23].condition.icon;
+  document.querySelector('#icon').src = icon
+  dataGagal.classList.add("display-none");
+  dataSuccess.classList.remove("display-none");
+  chart.style.display = 'block'
+  btnClose.style.display = 'block'
         const time = response.forecast.forecastday[0].hour.map(
           function(index){
               return index.time
@@ -80,28 +114,6 @@ myForm.addEventListener("submit", function (event) {
         chartKelembapan.update()
         chartkecepatanAngin.update()
         chartTekanan.update()
-      })
-    .catch((err) => {
-      showDataGagal();
-    });
-  }
-});
-
-function showDataSuccess(response) {
-  document.querySelector("#Negara").innerHTML = response.location.name;
-  document.querySelector("#tanggal").innerHTML = response.forecast.forecastday[0].date;
-  document.querySelector("#condition").innerHTML = response.forecast.forecastday[0].hour[23].condition.text;
-  var icon = 'https:'+ response.forecast.forecastday[0].hour[23].condition.icon;
-  document.querySelector('#icon').src = icon
-  document.querySelector("#temprature").innerHTML =
-  response.forecast.forecastday[0].hour[23].temp_c + "°C";
-  dataGagal.classList.add("display-none");
-  dataSuccess.classList.remove("display-none");
-}
-
-function showDataGagal() {
-  dataSuccess.classList.add("display-none");
-  dataGagal.classList.remove("display-none");
 }
 
 document.addEventListener("keyup", function(event) {
@@ -111,13 +123,66 @@ document.addEventListener("keyup", function(event) {
 });
 
 const btn = document.querySelector("#btn-submit");
+const btn1 = document.querySelector("#btn-submit1");
 
 btn.addEventListener("click", fetchHandler);
+btn1.addEventListener("click", fetchHandler);
 
 btnClose.addEventListener('click', function() {
-  popUpBg.classList.remove('active');
-  popUpChart.classList.remove('active');
+    chart.style.display = 'none'
+    dtNow.style.display = 'none'
+    btnClose.style.display = 'none'
 });
+
+var timeNow = new Date().getHours();
+
+function getData(){
+  var inputNegara1 = document.querySelector("#inputnegara").value;
+  var inputTanggal1 = document.querySelector("#inputtanggal").value;
+  const er1 = past7Days.includes(inputTanggal1)
+  if(inputNegara1 == "" || inputTanggal1 == ""){
+    Swal.fire({
+      icon: "warning",
+      title: "Perhatian",
+      text: "Harap mengisi Nama kota dan tanggalnya dulu!!",
+    });
+    return false;
+  }else if(er1 === false){
+    Swal.fire({
+      icon: "warning",
+      title: "Attention",
+      text: "Please enter the date within the last 7 days!!",
+    });
+  }
+  else{
+  fetch(
+    `https://weatherapi-com.p.rapidapi.com/history.json?q=${inputNegara1}&dt=${inputTanggal1}`,options)
+    .then((res) => res.json())
+    .then((response) => {
+      hideLoading()
+      dtNow.style.display = 'block';
+        var icon1 = 'https:'+ response.forecast.forecastday[0].hour[timeNow].condition.icon;
+        document.querySelector('#icon1').src = icon1
+        document.querySelector(".condition1").innerHTML = response.forecast.forecastday[0].hour[timeNow].condition.text;    
+        document.querySelector(".kota1").innerHTML = response.location.name;  
+        document.querySelector(".tm").innerHTML = response.forecast.forecastday[0].hour[timeNow].time;  
+        document.querySelector(".temp1").innerHTML = response.forecast.forecastday[0].hour[timeNow].temp_c;  
+        document.querySelector(".hum1").innerHTML = response.forecast.forecastday[0].hour[timeNow].humidity;  
+        document.querySelector(".wind").innerHTML = response.forecast.forecastday[0].hour[timeNow].wind_kph;  
+        document.querySelector(".pres").innerHTML = response.forecast.forecastday[0].hour[timeNow].pressure_mb;  
+        document.querySelector(".rain").innerHTML = response.forecast.forecastday[0].hour[timeNow].chance_of_rain; 
+    })
+    .catch((err) => {
+        Swal.fire({
+          icon: "warning",
+          title: "Attention",
+          text: "The country/city you are looking for was not found!!",
+        });
+        return false;
+    });
+  }
+}
+
 const loader = document.querySelector("#loading");
 const loader2 = document.querySelector("#loadbackground");
 
@@ -127,7 +192,7 @@ function displayLoading() {
     setTimeout(() => {
       loader.classList.remove("display");
       loader2.classList.remove("display");
-  }, 3000);
+  }, 2000);
 }
 
 function hideLoading() {
@@ -222,3 +287,5 @@ options: {
   }
 }
 });
+
+
